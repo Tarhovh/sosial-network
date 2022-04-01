a5f69be8fc1dda4ab2b0c25e4d89396f5314fbf8
Tarhovh <tarhovh@gmail.com>
2022-04-01 11:41:36 +0400

    firestore indef file changed

---
+++ src/myFireBase/index.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDaTcGszQUQBGvzehp7dspQpYvk6On07L4",
+  authDomain: "sosial-network-e2964.firebaseapp.com",
+  projectId: "sosial-network-e2964",
+  storageBucket: "sosial-network-e2964.appspot.com",
+  messagingSenderId: "267114802245",
+  appId: "1:267114802245:web:187c89edc13f60feb3c72c"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const myFireStore = firebase.firestore();
+
+export default myFireStore;
